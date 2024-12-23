f2988143dd9557d83c2391bb2fde56b2e3c2208a
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-12-23 13:31:14 +0530

    working on digital clock

---
+++ digitalClock.js
@@ -0,0 +1,14 @@
+//Selectors
+const hours = document.getElementById("#hours");
+const minutes = document.getElementById("#minutes");
+const seconds = document.getElementById("#seconds");
+
+const currentDate = new Date();
+console.log("Current Date is: ", currentDate)
+// console.log("Hours is: ", currentDate.getHours());
+// console.log("Minutes is: ", currentDate.getMinutes());
+// console.log("Seconds is: ", currentDate.getSeconds());
+
+// hours.textContent = currentDate.getHours();
+//hours.innerText = currentDate.getHours();
+console.log(hours)
